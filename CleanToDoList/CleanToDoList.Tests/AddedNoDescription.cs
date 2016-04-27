736c4c31781fe11533ae8684786c38cde5d56e28
mbuenoferrer <mbuenoferrer@gmail.com>
2016-04-27 11:05:45 +0200

    Third day

---
+++ CleanToDoList/CleanToDoList.Tests/AddedNoDescription.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CleanToDoLIst.Domain.UseCases;
+using CleanToDoLIst.Domain.Repositories;
+using CleanToDoLIst.Data.Repositories;
+using CleanToDoLIst.Domain.Entities;
+using Newtonsoft.Json;
+
+namespace CleanToDoList.Tests
+{
+    [TestClass]
+    public class AddedNoDescription
+    {
+        string path = @"AddedNoDescription.json";
+        [TestMethod]
+        public void GivenTaskWithoutDescriptionAndNoPreviousTaskWhenAddTaskThenTaskIsNotPersisted()
+        {
+            AddTaskRequest request = GivenTaskWithoutDescriptionAndNoPreviousTask();
+            WhenAddTask(request);
+            ThenTaskIsNotPersisted(request);
+        }
+
+
+
+        private AddTaskRequest GivenTaskWithoutDescriptionAndNoPreviousTask()
+        {
+            string description = "";
+            DateTime date = DateTime.Now.AddDays(3);
+
+            return new AddTaskRequest() { Description = description, DueDate = date };
+        }
+
+        private void WhenAddTask(AddTaskRequest request)
+        {
+            IToDoTaskRepository toDoTaskRepository = new ToDoTaskRepository(path);
+            AddTaskUseCase useCase = new AddTaskUseCase(toDoTaskRepository);
+            useCase.Add(request);
+        }
+
+        private void ThenTaskIsNotPersisted(AddTaskRequest request)
+        {
+            Assert.IsFalse(System.IO.File.Exists(path));
+
+        }
+    }
+}
---
+++ CleanToDoList/CleanToDoList.Tests/AddedPastDate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CleanToDoLIst.Domain.UseCases;
+using CleanToDoLIst.Domain.Repositories;
+using CleanToDoLIst.Data.Repositories;
+using Newtonsoft.Json;
+using CleanToDoLIst.Domain.Entities;
+
+namespace CleanToDoList.Tests
+{
+    [TestClass]
+    public class AddedPastDate
+    {
+        string path = @"AddedPastDate.json";
+        [TestMethod]
+        public void GivenTaskWithPastDateWhenAddTaskThenTaskIsNotPersisted()
+        {
+            AddTaskRequest givenTask = GivenTaskWithPastDate();
+            WhenAddTask(givenTask);
+            ThenTaskIsNotPersisted(givenTask);
+
+        }
+        private AddTaskRequest GivenTaskWithPastDate()
+        {
+            string description = "Esta es una descripción";
+            DateTime pastDate = DateTime.Now.AddDays(-1);
+            AddTaskRequest addTaskRequest = new AddTaskRequest { Description = description, DueDate = pastDate };
+            return addTaskRequest;
+        }
+
+        private void WhenAddTask(AddTaskRequest givenTask)
+        {
+            IToDoTaskRepository toDoTaskRepository = new ToDoTaskRepository(path);
+            AddTaskUseCase useCase = new AddTaskUseCase(toDoTaskRepository);
+            useCase.Add(givenTask);
+        }
+
+        private void ThenTaskIsNotPersisted(AddTaskRequest givenTask)
+        {
+            Assert.IsFalse(System.IO.File.Exists(path));
+        }
+
+
+
+
+    }
+}
